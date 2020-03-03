0e55c24ce06d8e2a776ac6f922dcb097783d88a3
Steffi LaZerte <steffi@steffi.ca>
2020-03-03 11:00:16 -0600

    Simplify run_all

--- run_all.R
+++ run_all.R
@@ -16,41 +16,35 @@
 # Create aquifer factsheets
 ##############################
 
-# Load Packages and Functions --------------------------------------------------
+# Update Data Sources -----------------------------------------------------
+# NOTE: Sometimes the downloads don't work on the first try, if you get an error,
+#       walk through the script and re-run lines which give you an error the first time
+source("01_download.R") # You don't have to run this every time
 
-# Check map and piperplots for correct names, fix issues with upper/lower case
-# and leading zeros
-checknames <- TRUE
+# Prepare Data and Figures ------------------------------------------------
+# These may take a while, depending how many aquifers have been selected
+source("02_load.R")
 
-source("00_functions.R")
-source("00_header.R")
 
 # Specify aquifers to run -------------------------------------------------
 
 # Large run - Run all Aquifers that have a Map file
-aquifers <- as.numeric(stringr::str_extract(list.files("./figures/maps/"), "[0-9]{4}"))
+
+# aquifers <- as.numeric(str_extract(list.files("./figures/maps/"), "[0-9]{4}"))
 
 # Specify a Range of aquifers (from the above set)
 # aquifers <- aquifers[aquifers >= 300 & aquifers <= 400]
 
 # Specific specific aquifers
-# aquifers <- c(6, 8, 353, 662, 738, 750, 15, 157)
-
-
-# Update Data Sources -----------------------------------------------------
-# NOTE: Sometimes the downloads don't work on the first try, if you get an error,
-#       walk through the script and re-run lines which give you an error the first time
-source("00_download.R") # You don't have to run this every time
-
-# Prepare Data and Figures ------------------------------------------------
-# These may take a while, depending how many aquifers have been selected
-source("01_load.R")
-source("02_clean.R")
-
+aquifers <- c(6, 8, 353, 662, 738, 750, 15, 157)
 delete_old <- TRUE   # Delete all old figures before rerunning?
-source("03_output.R")
 
-# Create all aquifer factsheets:
+source("03_clean.R")
+source("04_output.R")
+
+# Create aquifer factsheets -----------------------------------------------
+
+# Create all aquifer factsheets
 p <- progress_estimated(length(aquifers))
 for (a in aquifers) {
   factsheet(a, draft = TRUE)
@@ -58,14 +52,8 @@ for (a in aquifers) {
 }
 
 # Create a single factsheet
-factsheet(1156)
-factsheet(353)
-factsheet(21) #Example with many wells
-
-factsheet(98)
-factsheet(219)
-
-factsheet(662)
+factsheet(608, draft = TRUE)
+factsheet(21)
 
 # Create a single factsheet with only page 1
 factsheet(15, draft = FALSE, pages = 1)
