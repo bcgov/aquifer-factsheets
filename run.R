e3331b3deaa8c6325d5c19639189fd3a8c6617fe
Steffi LaZerte <steffi@steffi.ca>
2023-04-04 15:29:03 -0500

    Add parallel workers to run

--- run.R
+++ run.R
@@ -9,13 +9,15 @@
 # stations_dl(quiet = TRUE)
 
 
-targets::tar_make()
+targets::tar_make(reporter = "verbose_positives")
 # targets::tar_make_clustermq(workers = 2) # nolint
-# targets::tar_make_future(workers = 2) # nolint
+
+targets::tar_make_future(workers = 10,
+                         reporter = "verbose_positives")
 
 # Troubleshooting
 # - add `browser()` to function then run:
-targets::tar_make(callr_function = NULL)
+targets::tar_make(callr_function = NULL, reporter = "verbose_positives")
 
 # Find warnings in specific targets
 targets::tar_meta(fields = warnings, complete_only = TRUE)
